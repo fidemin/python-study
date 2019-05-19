8b3183f935bf45164049dc385584fed7b1f2575e
Yunhong Min <yhmin84@gmail.com>
2019-05-19 12:26:05 +0900

    garbage collect study

---
+++ personal_practice/garbage_collection/ref.py
@@ -0,0 +1,14 @@
+import sys
+
+# 2 references, 1 from the foo var and 1 from getrefcount
+foo = []
+print(sys.getrefcount(foo))
+
+def bar(a):
+    # 4 references
+    # from the foo var, function argument, getrefcount and Python's function stack
+    print(sys.getrefcount(a))
+
+bar(foo)
+# 2 references, the function scope is destroyed
+print(sys.getrefcount(foo))
