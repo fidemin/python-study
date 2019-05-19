52fa0af7fd3c9635381386b087172ff5443b5a27
Yunhong Min <yhmin84@gmail.com>
2019-05-19 14:54:51 +0900

    simple coroutine example

--- personal_practice/coroutine/coroutine_ex.py
+++ personal_practice/coroutine/coroutine_ex.py
@@ -3,7 +3,20 @@ def value_changer(start_value):
     while True:
         start_value = yield start_value
 
+
+def test():
+    a = yield "1"
+    b = yield "2"
+    print(a, b)
+    yield (a, b)
+
+
 if __name__ == "__main__":
     vc = value_changer("1")
     print(next(vc))
     print(vc.send("2"))
+
+    t = test()
+    print(next(t))
+    print(t.send("good1"))
+    print(t.send("good2"))
